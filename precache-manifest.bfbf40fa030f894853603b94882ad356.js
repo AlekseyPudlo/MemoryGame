46f974b6924129d2e40f6faca1a0a6a019bc86a8
Oleksii Pudlo <pudloaleksey@gmail>
2019-02-28 20:54:08 +0100

    Updates

---
+++ precache-manifest.bfbf40fa030f894853603b94882ad356.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MemoryGame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fb083a730414ee73c157",
+    "url": "/MemoryGame/static/js/runtime~main.fb083a73.js"
+  },
+  {
+    "revision": "d3e601474ca3d4ac1b49",
+    "url": "/MemoryGame/static/js/main.d3e60147.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/MemoryGame/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "d3e601474ca3d4ac1b49",
+    "url": "/MemoryGame/static/css/main.1e92ad99.chunk.css"
+  },
+  {
+    "revision": "26961f959d96ffd3abad0b95941501e1",
+    "url": "/MemoryGame/index.html"
+  }
+];
