adb90ab8f19d2a06352e02e777fd54d0021f64a6
Oleksii Pudlo <pudloaleksey@gmail>
2019-03-01 13:47:57 +0100

    Added select card functionaliy, changed card element from <div> to <img>

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import "../css/Card.css";
 
-const Card = ({ key, name, img }) => {
-    var divStyle = {
-        backgroundImage: 'url(' + img + ')'
-    }
-    return(
-        /* The img element commented below is to remember possibility of
+class Card extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			selected: false
+		};
+	}
+
+	handleClick = () => {
+		this.setState({
+			selected: !this.state.selected
+		});
+	};
+
+	render() {
+		const { key, name, img } = this.props;
+        const imgClasses = this.state.selected ? "card selected" : "card";
+        
+		return (
+			/* The "img" or "div" element commented below is to remember possibility of
             using image not as a beckground-image of css, maybe this usage
             will be more appropriate for card flipping option... */
-        /* <img src={img} /> */
-        <div className="card" key={key} name={name} style={divStyle}>
-
-        </div>
-    );
+			<img
+				alt=""
+				src={img}
+				className={imgClasses}
+				key={key}
+				name={name}
+				onClick={this.handleClick}
+			/>
+        /*  <div
+				className="card"
+				key={key}
+				name={name}
+				style={{
+					backgroundImage: "url(" + img + ")"
+				}}
+				onClick={this.handleClick}
+			/> */
+		);
+	}
 }
 
 export default Card;
